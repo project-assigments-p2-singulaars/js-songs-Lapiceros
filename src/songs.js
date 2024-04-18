5988ee8ab28c80608060c8a70ec03fa15eb6a403
julio <jcsmerubia@gmail.com>
2024-04-18 08:57:37 +0200

    only until exercise 5 I will finish it later,

--- src/songs.js
+++ src/songs.js
@@ -1,6 +1,7 @@
-//Exercise 1: Get the array of all Artists.
 function getAllArtists(array){
-    let result = '???';
+    let result = array.map(song => {
+        return song.artist
+    });
     //console.log("Exercise 1 ->", result);
     return result;
 };
@@ -8,21 +9,62 @@ function getAllArtists(array){
 //Exercise 2: Get the songs of a certain artist
 function getSongsFromArtist(array, artist){
     //Write your code here
+    let songsFromArtist = array.filter(song => song.artist === artist);
+    return songsFromArtist;
 };
 
 //Exercise 3: Alphabetic order by title
-function orderAlphabetically(){
+function orderAlphabetically(array){
     //Write your code here
+    const newArray = array.slice();
+
+    newArray.sort((a, b) => {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+        if (titleA < titleB) {
+            return -1;
+        }
+        if (titleA > titleB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    const soloStrings = newArray.map(objeto => objeto.title);
+
+    return soloStrings.slice(0, 10);
+    ;
 };
 
 //Exercise 4: Order by year, ascending
-function orderByYear(){
+function orderByYear(songs){
     //Write your code here
+    const newSongs = songs.slice();
+
+    newSongs.sort((a, b) => {
+        if (a.year !== b.year) {
+            return a.year - b.year;
+        } else {
+            const titleA = a.title.toLowerCase();
+            const titleB = b.title.toLowerCase();
+            if (titleA < titleB) {
+                return -1;
+            }
+            if (titleA > titleB) {
+                return 1;
+            }
+            return 0;
+        }
+    });
+
+    return newSongs;
 };
 
 //Exercise 5: Filter songs by genre
-function songsByGenre() {
+function songsByGenre(songs, filter) {
     //Write your code here
+    const filteredSongs = songs.filter((song) => song.genre.includes(filter));
+    return filteredSongs;
 };
 
 //Exercise 6: Modify the duration of songs to seconds
